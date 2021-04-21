1feb71fe6e2ed31b3c799f89872ea2a52e08c828
apratt7891 <apratt7891@gmail.com>
2021-04-21 13:24:33 -0600

    add js

---
+++ assets/js/weather.js
@@ -0,0 +1,11 @@
+let city = "";
+
+let searchCity = $("#search-city");
+let searchBtn = $("#search-btn");
+let currentCity = $(".current-weather");
+let currentTemp = $("#current-temp");
+let currentWind = $("#wind");
+let currentHumidity = $("#Humidity");
+let currentUVIndex = $("#uv-index");
+
+var APIKey = "b953fd1506cd776c7f6b755564e6c8af";
