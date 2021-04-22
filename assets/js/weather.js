5c5ff8bb59d00568084050faed2770cbee2b268b
apratt7891 <apratt7891@gmail.com>
2021-04-21 19:03:26 -0600

    add some js

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,11 +1,24 @@
-let city = "";
+let currentCity = "";
+let API = 'b953fd1506cd776c7f6b755564e6c8af';
 
-let searchCity = $("#search-city");
-let searchBtn = $("#search-btn");
-let currentCity = $(".current-weather");
-let currentTemp = $("#current-temp");
-let currentWind = $("#wind");
-let currentHumidity = $("#Humidity");
-let currentUVIndex = $("#uv-index");
 
-var APIKey = "b953fd1506cd776c7f6b755564e6c8af";
+
+
+$(document).ready(function () {
+
+    $(".searchBtn").on("click", function() {
+    let searchCity = $("#search-city").val();
+    currentCity =  $("#search-city").val();
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + API;
+    fetch(weatherURL)
+    .then(function(response) {
+        return response.json();
+    })
+    })
+})
+
+
+
+
+
+
