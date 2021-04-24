8188d3eb73fb7cedc9d787f278d7f7726a192172
apratt7891 <apratt7891@gmail.com>
2021-04-23 18:38:44 -0600

    add api and local storage

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,24 +1,25 @@
-let currentCity = "";
-let API = 'b953fd1506cd776c7f6b755564e6c8af';
+let apiKey = 'b953fd1506cd776c7f6b755564e6c8af';
+let searchCity = $("#search-city");
 
 
-
-
-$(document).ready(function () {
-
-    $(".searchBtn").on("click", function() {
-    let searchCity = $("#search-city").val();
-    currentCity =  $("#search-city").val();
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + API;
-    fetch(weatherURL)
+$(".searchBtn").on("click", function(event) {
+   event.preventDefault();
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + searchCity + '&units=imperial&appid=' + apiKey)
     .then(function(response) {
         return response.json();
     })
-    })
-})
+    .then(function(response) {
+   
+let searchCity = document.querySelector('#search-city').value;
+localStorage.setItem('city', JSON.stringify(searchCity));
 
+let currentCityTempEl = document.querySelector('#current-temp')
+currentCityTempEl.textContent = 'Current Temperature :' + response.maintemp + '°F';
 
+let currentCityWindEl = document.querySelector('#wind')
+currentCityWindEl.textContent = 'Wind :' + response + 'MPH';
 
+let currentCityHumidiyEl = document.querySelector('#Humidity')
+currentCityHumidiyEl.textContent = 'Humidity: ' + response + '%';
 
-
-
+})})
